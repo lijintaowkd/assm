6380a03cb9689a781393f633a43e9d91ac6480c4
lijintao <269500571@qq.com>
2017-07-13 11:09:26 +0800

    first Commit

---
+++ src/test/java/spittr/jdbc/JdbcConfig.java
@@ -0,0 +1,74 @@
+package spittr.jdbc;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import spittr.db.SpitterRepository;
+import spittr.db.SpittleRepository;
+import spittr.db.jdbc.JdbcSpitterRepository;
+import spittr.db.jdbc.JdbcSpittleRepository;
+
+@Configuration
+public class JdbcConfig {
+
+  @Profile("dev")	
+  @Bean
+  public DataSource embeddedDataSource() {
+    return new EmbeddedDatabaseBuilder()
+      .setType(EmbeddedDatabaseType.H2)
+      .addScripts("classpath:spittr/db/jdbc/schema.sql", "classpath:spittr/db/jdbc/test-data.sql")
+      .build();
+  }
+  @Bean
+  @Profile("qa")
+  public DataSource Data() {
+    BasicDataSource ds = new BasicDataSource();
+    ds.setDriverClassName("com.mysql.jdbc.Driver");
+    ds.setUrl("jdbc:mysql://127.0.0.1:3306/spittr");
+    ds.setUsername("root");
+    ds.setPassword("555769");
+    ds.setInitialSize(5);
+    ds.setMaxActive(10);
+    return ds;
+  }
+ 
+  @Bean
+  @Profile("prod")
+  public DataSource jndiDataSource() {
+    JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+    jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+    jndiObjectFactoryBean.setResourceRef(true);
+    jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+    return (DataSource) jndiObjectFactoryBean.getObject();
+  }
+  @Bean
+  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+  
+  @Bean
+  public SpitterRepository spitterRepository(JdbcTemplate jdbcTemplate) {
+    return new JdbcSpitterRepository(jdbcTemplate);
+  }
+
+  @Bean
+  public SpittleRepository spittleRepository(JdbcTemplate jdbcTemplate) {
+    return new JdbcSpittleRepository(jdbcTemplate);
+  }
+  
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+}
---
+++ src/test/resources/spittr/db/jdbc/test-data.sql
@@ -0,0 +1,20 @@
+insert into Spitter (username, password, fullname, email, updateByEmail) values ('habuma', 'password', 'Craig Walls', 'craig@habuma.com', false);
+insert into Spitter (username, password, fullname, email, updateByEmail) values ('mwalls', 'password', 'Michael Walls', 'mwalls@habuma.com', true);
+insert into Spitter (username, password, fullname, email, updateByEmail) values ('chuck', 'password', 'Chuck Wagon', 'chuck@habuma.com', false);
+insert into Spitter (username, password, fullname, email, updateByEmail) values ('artnames', 'password', 'Art Names', 'art@habuma.com', true);
+
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (1, 'This is a test spittle message', '2012-06-09 22:00:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (1, 'This is another test spittle message', '2012-06-09 22:10:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (1, 'This is a third test spittle message', '2012-07-04 23:30:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (2, 'Hello from Chuck!', '2012-03-25 12:15:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Hello from Art!', '2012-03-25 12:15:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Hello again from Art!', '2012-03-25 12:25:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Hola from Arthur!', '2012-03-25 12:35:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Buenos Dias from Art!', '2012-03-25 12:45:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Ni Hao from Art!', '2012-03-25 12:55:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Guten Tag from Art!', '2012-03-25 13:05:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Konnichi wa from Art!', '2012-03-25 13:15:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Buon giorno from Art!', '2012-03-25 13:25:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Bonjour from Art!', '2012-03-25 13:35:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'Aloha from Art!', '2012-03-25 13:45:00');
+INSERT INTO Spittle (spitter, message, postedTime) VALUES (4, 'God dag from Art!', '2012-03-25 13:55:00');
